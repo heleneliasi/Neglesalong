66b825c13e83269ff21d840452eb575caa024f01
milajotic <mijoa058@osloskolen.no>
2026-01-30 11:05:23 +0100

    endringer

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request, redirect, session
 import mariadb
-=======
-from flask import Flask,
-render_template, request, redirect, session
->>>>>>> d925abce4951de774f24f4b68c2491526555cb34
 from datetime import datetime
 
 app = Flask(__name__)
