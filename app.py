d925abce4951de774f24f4b68c2491526555cb34
heleneliasi <hedaa012@osloskolen.no>
2026-01-30 10:54:22 +0100

    test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, session
-import mysql.connector
+from flask import Flask,
+render_template, request, redirect, session
 from datetime import datetime
 
 app = Flask(__name__)
