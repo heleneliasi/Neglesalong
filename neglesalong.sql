467853a173547be6d3729917a5d40b773ab7880f
milajotic <mijoa058@osloskolen.no>
2026-03-26 11:25:22 +0100

    sql table update

--- neglesalong.sql
+++ neglesalong.sql
@@ -2,7 +2,8 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'user'
 );
 
 CREATE TABLE service (
